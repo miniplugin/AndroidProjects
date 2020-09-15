ded2053d64d0e91e36ac784c4a9aa643e2ff87df
boramcom <boramcom@daum.net>
2020-09-15 11:34:59 +0900

    메인 액티비티에 주석 추가

--- DoItAndroid/app/src/main/java/com/example/doitandroid/MainActivity.java
+++ DoItAndroid/app/src/main/java/com/example/doitandroid/MainActivity.java
@@ -31,7 +31,18 @@ public class MainActivity extends AppCompatActivity {
     public void AppLogin(View view) {
         Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show();
     }
-
+    /*
+    - Intent 는 실행할 인스턴스를 담는 봉투역할.
+            - 실행은 startActivity(인텐트명);
+    - 오버로드 Intent(출력할 위치this, 출력할 콘텐츠activity);
+    - 오버로드 Intent(실행할 액션action_view, 파싱정보http:,tel:);
+    - 안드로이드 액티비티 화면이동의 특징(아래-웹페이지이동과는 틀림)
+    - 액티비티(화면)에서 다른 액티비티(화면) 호출한다고해서,
+                - 기존 액티비티(화면)이 종료되는게 아닙니다.
+                - 기존 액티비티(화면) 위쪽에 호출된 액티비티(화면)이 올라타는 구조
+    - 그래서 서브액티비티에서 finish()메서드를 실행하면,
+    - 현재 액티비티가 종료되고, 밑에 있던 메인액티비티가 나타납니다.
+    */
     public void NaverGoUrl(View view) {
         Intent naverGoUrl = new Intent(Intent.ACTION_VIEW, Uri.parse("http://naver.com"));
         startActivity(naverGoUrl);
